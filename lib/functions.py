d4e778b568c16a38adaa40c07a5c5e4c3e57ca82
Manny <mhfernan35277@gmail.com>
2024-07-30 01:03:55 -0700

    add file

--- lib/functions.py
+++ lib/functions.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    print("Hello programmer!")
+    print("Hello, programmer!")
 
 def greet(name):
-    print(f"Hello {name}!")
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    print(f"Hello {name}!")
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
     return num1 + num2
